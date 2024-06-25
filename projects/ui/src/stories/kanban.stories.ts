c17d1d052ae06065e2e3339d869e6a4b169e6ef7
DineshCodeFlow <drawat@shorterloop.com>
2024-06-25 12:20:32 +0530

    Fixed html rendering when solutions are not present

--- projects/ui/src/stories/kanban.stories.ts
+++ projects/ui/src/stories/kanban.stories.ts
@@ -358,342 +358,143 @@ const kanbanDataWithSwimlanes = [
 
 const workflowItems = [
   {
-    opportunity: {
-      id: 1,
-      header: [
-        {
-          type: 'link', label: 'EP-1', event: ($event: any, column: any) => {
-            console.log("Added new experiment action performed: ", column, $event);
+    "opportunity": {
+      "id": 1,
+      "header": [],
+      "body": {
+        "description": "Conduct research and analysis on emerging trends in renewable energy and sustainable technologies"
+      },
+      "footer": [],
+      "borderColor": "#4784fc",
+      "action": {
+        "label": "+ Add Solution",
+        "icon": "<svg width=\"16\" height=\"16\" viewBox=\"0 0 16 16\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M2 2.667a.667.667 0 0 0-.662.588l-.005.078v4.334a2.333 2.333 0 0 0 2.206 2.33l.128.003h8.723l-2.195 2.195a.67.67 0 0 0-.055.88l.055.063a.67.67 0 0 0 .88.055l.063-.055 3.333-3.333a1 1 0 0 0 .065-.075l.047-.073.036-.076.024-.07.016-.078.005-.04.003-.06-.002-.05-.012-.084-.02-.074-.029-.074-.035-.065-.042-.061-.056-.063-3.333-3.333a.667.667 0 0 0-.998.88l.055.062 2.195 2.196H3.667a1 1 0 0 1-.996-.904l-.004-.096V3.333A.667.667 0 0 0 2 2.667\" fill=\"#573DF4\"/></svg>"
+      }
+    },
+    "solutions": [
+      {
+        "solution": {
+          "id": 1,
+          "header": [],
+          "body": {
+            "description": "Community Engagement Initiative"
+          },
+          "footer": [],
+          "borderColor": "#4784fc",
+          "action": {
+            "label": "+ Add Experiment",
+            "icon": "<svg width=\"16\" height=\"16\" viewBox=\"0 0 16 16\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M2 2.667a.667.667 0 0 0-.662.588l-.005.078v4.334a2.333 2.333 0 0 0 2.206 2.33l.128.003h8.723l-2.195 2.195a.67.67 0 0 0-.055.88l.055.063a.67.67 0 0 0 .88.055l.063-.055 3.333-3.333a1 1 0 0 0 .065-.075l.047-.073.036-.076.024-.07.016-.078.005-.04.003-.06-.002-.05-.012-.084-.02-.074-.029-.074-.035-.065-.042-.061-.056-.063-3.333-3.333a.667.667 0 0 0-.998.88l.055.062 2.195 2.196H3.667a1 1 0 0 1-.996-.904l-.004-.096V3.333A.667.667 0 0 0 2 2.667\" fill=\"#573DF4\"/></svg>"
           }
         },
-        { type: 'chip', value: [{ value: 'Tag 1', backgroundColor: '#FFF1F1', foregroundColor: '#D3272A' }, { value: 'Tag 2', backgroundColor: '#009042' }] },
-      ],
-      body: { description: 'To assess whether users perceive a high value from using Shorter Loop.' },
-      footer: [
-        { type: 'avatar', label: 'Dinesh Rawat', photo: 'https://picsum.photos/201' },
-        { type: 'budget', label: 'Budget', value: '$100' },
-        { type: 'timeFrame', label: 'Time Frame', value: '1 month' },
-      ],
-      borderColor: '#4784fc',
-      action: {
-        label: '+ Add Solution',
-        icon: SVG_ICON,
-        event: ($event: any, column: any) => {
-          console.log("Added new experiment action performed: ", column, $event);
-        }
-      },
-    },
-    solutions: [
-      {
-        solution:
-        {
-          id: 1,
-          header: [
-            {
-              type: 'link', label: 'EP-1', event: ($event: any, column: any) => {
-                console.log("Added new experiment action performed: ", column, $event);
-              }
-            },
-            { type: 'chip', value: [{ value: 'Tag 1', backgroundColor: '#FFF1F1', foregroundColor: '#D3272A' }, { value: 'Tag 2', backgroundColor: '#009042' }] },
-          ],
-          body: { description: 'Solution 1.1' },
-          footer: [
-            { type: 'avatar', label: 'Dinesh Rawat', photo: 'https://picsum.photos/201' },
-            { type: 'budget', label: 'Budget', value: '$100' },
-            { type: 'timeFrame', label: 'Time Frame', value: '1 month' },
-          ],
-          borderColor: '#4784fc',
-          action: {
-            label: '+ Add Experiment',
-            icon: SVG_ICON,
-            event: ($event: any, column: any) => {
-              console.log("Added new experiment action performed: ", column, $event);
-            }
-          }
-        }
-        , experiments: [
+        "experiments": [
           {
-            id: 1,
-            header: [
+            "id": 520,
+            "header": [
               {
-                type: 'link', label: 'EX-98', event: ($event: any, column: any) => {
-                  console.log("Added new experiment action performed: ", column, $event);
-                }
+                "type": "link",
+                "label": "EXP-1"
               },
-              { type: 'chip', value: [{ value: 'Tag 1', backgroundColor: '#FFF1F1', foregroundColor: '#D3272A' }, { value: 'Tag 2', backgroundColor: '#009042' }] },
-            ],
-            body: { description: 'Experiment 1.1.1' },
-            footer: [
-              { type: 'avatar', label: 'Dinesh Rawat', photo: 'https://picsum.photos/201' },
-              { type: 'budget', label: 'Budget', value: '$100' },
-              { type: 'timeFrame', label: 'Time Frame', value: '1 month' },
-            ],
-            borderColor: '#4784fc'
-          }, {
-            id: 1,
-            header: [
               {
-                type: 'link', label: 'EX-199', event: ($event: any, column: any) => {
-                  console.log("Added new experiment action performed: ", column, $event);
-                }
+                "type": "chip",
+                "value": []
+              }
+            ],
+            "body": {
+              "description": "etee"
+            },
+            "footer": [
+              {
+                "type": "budget",
+                "label": "Budget",
+                "value": "$50.00"
               },
-              { type: 'chip', value: [{ value: 'Tag 1', backgroundColor: '#FFF1F1', foregroundColor: '#D3272A' }, { value: 'Tag 2', backgroundColor: '#009042' }] },
+              {
+                "type": "timeFrame",
+                "label": "Time Frame",
+                "value": "1 month"
+              }
             ],
-            body: { description: 'Experiment 1.1.2' },
-            footer: [
-              { type: 'avatar', label: 'Dinesh Rawat', photo: 'https://picsum.photos/201' },
-              { type: 'budget', label: 'Budget', value: '$100' },
-              { type: 'timeFrame', label: 'Time Frame', value: '1 month' },
-            ],
-            borderColor: '#4784fc'
+            "borderColor": "#4784fc"
           },
           {
-            id: 1,
-            header: [
+            "id": 521,
+            "header": [
               {
-                type: 'link', label: 'EX-200', event: ($event: any, column: any) => {
-                  console.log("Added new experiment action performed: ", column, $event);
-                }
+                "type": "link",
+                "label": "EXP-2"
               },
-              { type: 'chip', value: [{ value: 'Tag 1', backgroundColor: '#FFF1F1', foregroundColor: '#D3272A' }, { value: 'Tag 2', backgroundColor: '#009042' }] },
-            ],
-            body: { description: 'Experiment 1.1.3' },
-            footer: [
-              { type: 'avatar', label: 'Dinesh Rawat', photo: 'https://picsum.photos/201' },
-              { type: 'budget', label: 'Budget', value: '$100' },
-              { type: 'timeFrame', label: 'Time Frame', value: '1 month' },
-            ],
-            borderColor: '#4784fc'
-          }
-        ]
-      },
-      {
-        solution:
-        {
-          id: 1,
-          header: [
-            {
-              type: 'link', label: 'EP-1', event: ($event: any, column: any) => {
-                console.log("Added new experiment action performed: ", column, $event);
+              {
+                "type": "chip",
+                "value": []
               }
+            ],
+            "body": {
+              "description": "Hilooo"
             },
-            { type: 'chip', value: [{ value: 'Tag 1', backgroundColor: '#FFF1F1', foregroundColor: '#D3272A' }, { value: 'Tag 2', backgroundColor: '#009042' }] },
-          ],
-          body: { description: 'Solution 1.2' },
-          footer: [
-            { type: 'avatar', label: 'Dinesh Rawat', photo: 'https://picsum.photos/201' },
-            { type: 'budget', label: 'Budget', value: '$100' },
-            { type: 'timeFrame', label: 'Time Frame', value: '1 month' },
-          ],
-          borderColor: '#4784fc',
-          action: {
-            label: '+ Add Experiment',
-            icon: SVG_ICON,
-            event: ($event: any, column: any) => {
-              console.log("Added new experiment action performed: ", column, $event);
-            }
-          }
-
-        }, experiments: [
-          {
-            id: 3,
-            header: [
+            "footer": [
               {
-                type: 'link', label: 'EX-98', event: ($event: any, column: any) => {
-                  console.log("Added new experiment action performed: ", column, $event);
-                }
+                "type": "budget",
+                "label": "Budget",
+                "value": "$20.00"
               },
-              { type: 'chip', value: [{ value: 'Tag 1', backgroundColor: '#FFF1F1', foregroundColor: '#D3272A' }, { value: 'Tag 2', backgroundColor: '#009042' }] },
-            ],
-            body: { description: 'Experiment 1.2.1' },
-            footer: [
-              { type: 'avatar', label: 'Dinesh Rawat', photo: 'https://picsum.photos/201' },
-              { type: 'budget', label: 'Budget', value: '$100' },
-              { type: 'timeFrame', label: 'Time Frame', value: '1 month' },
-            ],
-            borderColor: '#4784fc'
-          }, {
-            id: 4,
-            header: [
               {
-                type: 'link', label: 'EX-99', event: ($event: any, column: any) => {
-                  console.log("Added new experiment action performed: ", column, $event);
-                }
-              },
-              { type: 'chip', value: [{ value: 'Tag 1', backgroundColor: '#FFF1F1', foregroundColor: '#D3272A' }, { value: 'Tag 2', backgroundColor: '#009042' }] },
+                "type": "timeFrame",
+                "label": "Time Frame",
+                "value": "1 month"
+              }
             ],
-            body: { description: 'Experiment 1.2.2' },
-            footer: [
-              { type: 'avatar', label: 'Dinesh Rawat', photo: 'https://picsum.photos/201' },
-              { type: 'budget', label: 'Budget', value: '$100' },
-              { type: 'timeFrame', label: 'Time Frame', value: '1 month' },
-            ],
-            borderColor: '#4784fc'
+            "borderColor": "#4784fc"
           },
           {
-            id: 5,
-            header: [
+            "id": 522,
+            "header": [
               {
-                type: 'link', label: 'EX-100', event: ($event: any, column: any) => {
-                  console.log("Added new experiment action performed: ", column, $event);
-                }
+                "type": "link",
+                "label": "EXP-3"
               },
-              { type: 'chip', value: [{ value: 'Tag 1', backgroundColor: '#FFF1F1', foregroundColor: '#D3272A' }, { value: 'Tag 2', backgroundColor: '#009042' }] },
+              {
+                "type": "chip",
+                "value": []
+              }
             ],
-            body: { description: 'Experiment 1.2.3' },
-            footer: [
-              { type: 'avatar', label: 'Dinesh Rawat', photo: 'https://picsum.photos/201' },
-              { type: 'budget', label: 'Budget', value: '$100' },
-              { type: 'timeFrame', label: 'Time Frame', value: '1 month' },
+            "body": {
+              "description": "Conduct research and analysis on emerging trends in renewable energy and sustainable technologies"
+            },
+            "footer": [
+              {
+                "type": "budget",
+                "label": "Budget",
+                "value": "$200.00"
+              },
+              {
+                "type": "timeFrame",
+                "label": "Time Frame",
+                "value": "1 month"
+              }
             ],
-            borderColor: '#4784fc'
+            "borderColor": "#4784fc"
           }
         ]
       }
     ]
   },
   {
-    opportunity: {
-      id: 2,
-      header: [
-        {
-          type: 'link', label: 'EP-400', event: ($event: any, column: any) => {
-            console.log("Added new experiment action performed: ", column, $event);
-          }
-        },
-        { type: 'chip', value: [{ value: 'Tag 1', backgroundColor: '#FFF1F1', foregroundColor: '#D3272A' }, { value: 'Tag 2', backgroundColor: '#009042' }] },
-      ],
-      body: { description: 'Opportunity 2' },
-      footer: [
-        { type: 'avatar', label: 'Dinesh Rawat', photo: 'https://picsum.photos/201' },
-        { type: 'budget', label: 'Budget', value: '$100' },
-        { type: 'timeFrame', label: 'Time Frame', value: '1 month' },
-      ],
-      borderColor: '#4784fc',
-      action: {
-        label: '+ Add Solution',
-        icon: SVG_ICON,
-        event: ($event: any, column: any) => {
-          console.log("Added new experiment action performed: ", column, $event);
-        }
+    "opportunity": {
+      "id": 2,
+      "header": [],
+      "body": {
+        "description": "Collaborate with cross-functional teams to develop strategies for integrating green technologies into everyday applications.\n"
+      },
+      "footer": [],
+      "borderColor": "#4784fc",
+      "action": {
+        "label": "+ Add Solution",
+        "icon": "<svg width=\"16\" height=\"16\" viewBox=\"0 0 16 16\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M2 2.667a.667.667 0 0 0-.662.588l-.005.078v4.334a2.333 2.333 0 0 0 2.206 2.33l.128.003h8.723l-2.195 2.195a.67.67 0 0 0-.055.88l.055.063a.67.67 0 0 0 .88.055l.063-.055 3.333-3.333a1 1 0 0 0 .065-.075l.047-.073.036-.076.024-.07.016-.078.005-.04.003-.06-.002-.05-.012-.084-.02-.074-.029-.074-.035-.065-.042-.061-.056-.063-3.333-3.333a.667.667 0 0 0-.998.88l.055.062 2.195 2.196H3.667a1 1 0 0 1-.996-.904l-.004-.096V3.333A.667.667 0 0 0 2 2.667\" fill=\"#573DF4\"/></svg>"
       }
     },
-    solutions: [
-      {
-        solution:
-        {
-          id: 1,
-          header: [
-            {
-              type: 'link', label: 'EP-1000', event: ($event: any, column: any) => {
-                console.log("Added new experiment action performed: ", column, $event);
-              }
-            },
-            { type: 'chip', value: [{ value: 'Tag 1', backgroundColor: '#FFF1F1', foregroundColor: '#D3272A' }, { value: 'Tag 2', backgroundColor: '#009042' }] },
-          ],
-          body: { description: 'Solution 1.2' },
-          footer: [
-            { type: 'avatar', label: 'Dinesh Rawat', photo: 'https://picsum.photos/201' },
-            { type: 'budget', label: 'Budget', value: '$100' },
-            { type: 'timeFrame', label: 'Time Frame', value: '1 month' },
-          ],
-          borderColor: '#4784fc',
-          action: {
-            label: '+ Add Experiment',
-            icon: SVG_ICON,
-            event: ($event: any, column: any) => {
-              console.log("Added new experiment action performed: ", column, $event);
-            }
-          }
-        }
-        , experiments: [
-          {
-            id: 1,
-            header: [
-              {
-                type: 'link', label: 'EX-98', event: ($event: any, column: any) => {
-                  console.log("Added new experiment action performed: ", column, $event);
-                }
-              },
-              { type: 'chip', value: [{ value: 'Tag 1', backgroundColor: '#FFF1F1', foregroundColor: '#D3272A' }, { value: 'Tag 2', backgroundColor: '#009042' }] },
-            ],
-            body: { description: 'Experiment 1.1.1' },
-            footer: [
-              { type: 'avatar', label: 'Dinesh Rawat', photo: 'https://picsum.photos/201' },
-              { type: 'budget', label: 'Budget', value: '$100' },
-              { type: 'timeFrame', label: 'Time Frame', value: '1 month' },
-            ],
-            borderColor: '#4784fc'
-          }
-        ]
-      },
-      {
-        solution:
-        {
-          id: 1,
-          header: [
-            {
-              type: 'link', label: 'EP-1', event: ($event: any, column: any) => {
-                console.log("Added new experiment action performed: ", column, $event);
-              }
-            },
-            { type: 'chip', value: [{ value: 'Tag 1', backgroundColor: '#FFF1F1', foregroundColor: '#D3272A' }, { value: 'Tag 2', backgroundColor: '#009042' }] },
-          ],
-          body: { description: 'Solution 2.2' },
-          footer: [
-            { type: 'avatar', label: 'Dinesh Rawat', photo: 'https://picsum.photos/201' },
-            { type: 'budget', label: 'Budget', value: '$100' },
-            { type: 'timeFrame', label: 'Time Frame', value: '1 month' },
-          ],
-          borderColor: '#4784fc',
-          action: {
-            label: '+ Add Solution',
-            icon: SVG_ICON,
-            event: ($event: any, column: any) => {
-              console.log("Added new experiment action performed: ", column, $event);
-            }
-          }
-        },
-        experiments: [
-          {
-            id: 3,
-            header: [
-              {
-                type: 'link', label: 'EX-98', event: ($event: any, column: any) => {
-                  console.log("Added new experiment action performed: ", column, $event);
-                }
-              },
-              { type: 'chip', value: [{ value: 'Tag 1', backgroundColor: '#FFF1F1', foregroundColor: '#D3272A' }, { value: 'Tag 2', backgroundColor: '#009042' }] },
-            ],
-            body: { description: 'Experiment 1.2.1' },
-            footer: [
-              { type: 'avatar', label: 'Dinesh Rawat', photo: 'https://picsum.photos/201' },
-              { type: 'budget', label: 'Budget', value: '$100' },
-              { type: 'timeFrame', label: 'Time Frame', value: '1 month' },
-            ],
-            borderColor: '#4784fc'
-          }, {
-            id: 4,
-            header: [
-              {
-                type: 'link', label: 'EX-99', event: ($event: any, column: any) => {
-                  console.log("Added new experiment action performed: ", column, $event);
-                }
-              },
-              { type: 'chip', value: [{ value: 'Tag 1', backgroundColor: '#FFF1F1', foregroundColor: '#D3272A' }, { value: 'Tag 2', backgroundColor: '#009042' }] },
-            ],
-            body: { description: 'Experiment 1.2.2' },
-            footer: [
-              { type: 'avatar', label: 'Dinesh Rawat', photo: 'https://picsum.photos/201' },
-              { type: 'budget', label: 'Budget', value: '$100' },
-              { type: 'timeFrame', label: 'Time Frame', value: '1 month' },
-            ],
-            borderColor: '#4784fc'
-          }
-        ]
-      }
-    ]
-  },
+
+    "solutions": []
+  }
 ];
 
 const tableHeaders = [{
